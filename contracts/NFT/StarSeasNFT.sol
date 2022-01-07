727a632d46d087b9e8f2549c577426caaf465ff0
dragonyu1010 <provic0099@gmail.com>
2022-01-08 04:26:45 +0800

    add burn() method into NFT contract

--- contracts/NFT/StarSeasNFT.sol
+++ contracts/NFT/StarSeasNFT.sol
@@ -8,6 +8,7 @@ import "../AccessControl/StarSeasAccessControl.sol";
 contract StarSeasNFT is ERC721 {
     using SafeMath for uint256; 
     uint256 public tokenAmount;
+    uint256 public tokenIdPointer;
     mapping(uint256 => string) public tokenHashList;
     string public baseTokenURI;
 
@@ -17,8 +18,10 @@ contract StarSeasNFT is ERC721 {
         accessControls = accessControls_;
     }
     function mint(string calldata _metaData) public returns(uint) {
-        uint256 tokenId = tokenAmount;
-        _mint(msg.sender, tokenId);
+        uint256 tokenId;
+        tokenId = tokenIdPointer;
+        tokenIdPointer = tokenIdPointer.add(1);
+        _safeMint(msg.sender, tokenId);
         tokenHashList[tokenId] = _metaData;
         tokenAmount = tokenAmount.add(1);
         return tokenId;
@@ -28,5 +31,18 @@ contract StarSeasNFT is ERC721 {
     }
     function setBaseURI(string memory baseURI) public {
         baseTokenURI = baseURI;
+    }
+    function burn(uint256 _tokenId) public returns(bool) {
+        address operator = _msgSender();
+        require(
+            ownerOf(_tokenId) == operator || isApproved(_tokenId, operator) || accessControls.hasAdminRole(_msgSender()),
+            "StarSeasNFT.burn: Only garment owner or approved"
+        );
+        _burn(_tokenId);
+        tokenAmount = tokenAmount.sub(1);
+        return true;
+    }
+    function isApproved(uint256 _tokenId, address _operator) public view returns (bool) {
+        return isApprovedForAll(ownerOf(_tokenId), _operator) || getApproved(_tokenId) == _operator;
     }
 }
--- test/StarSeasNFT.test.js
+++ test/StarSeasNFT.test.js
@@ -5,30 +5,38 @@ require('chai')
 const {assert} = require('chai')
 
 const StarSeasNft = artifacts.require('./NFT/StarSeasNFT.sol')
-
+const AccessControl = artifacts.require('./AccessControl/StarSeasAccessControl.sol')
 contract('StarSeasNFT', (accounts) => {
-    let starSeasNFT, res
+    let starSeasNFT, accessControl, res
     before(async() => {
         starSeasNFT = await StarSeasNft.deployed()
+        accessControl = await AccessControl.deployed()
     })
     it('mint NFT', async() => {
         res = await starSeasNFT.mint("token 1")
-        console.log(res.logs[0].args)
-        await starSeasNFT.mint("token 2")
-        console.log(res.logs[0].args)
+        assert.equal(res.logs[0].args.to, accounts[0], 'To address is correct')
+        res = await starSeasNFT.mint("token 2")
+        assert.equal(res.logs[0].args.tokenId, 1, 'Token Id is correct')
         await starSeasNFT.mint("token 3")
     })
     it('set base uri', async() => {
         await starSeasNFT.setBaseURI('https://ipfs/')
         res = await starSeasNFT.tokenURI(0)
-        console.log(res)
+        assert.equal(res, 'https://ipfs/0', 'Token 0 URI is correct')
         res = await starSeasNFT.tokenURI(1)
-        console.log(res)
+        assert.equal(res, 'https://ipfs/1', 'Token 0 URI is correct')
         res = await starSeasNFT.tokenURI(2)
-        console.log(res)
+        assert.equal(res, 'https://ipfs/2', 'Token 0 URI is correct')
     })
     it('Access Controller Test', async() => {
         res = await starSeasNFT.accessControls.call()
-        console.log(res)
+        assert.equal(res, accessControl.address, 'StarSeasNFT contract has admin role')
+    })
+    it('burn test', async() => {
+        res = await starSeasNFT.tokenAmount()
+        assert.equal(res, 3, 'Amount of NFT is correct before burnning')
+        await starSeasNFT.burn(0)
+        res = await starSeasNFT.tokenAmount()
+        assert.equal(res, 2, 'Amount of NFT is correct after burnning')
     })
 })
